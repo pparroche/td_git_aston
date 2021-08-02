6ea08a67dded4b1dffe529254104e94b2c188c99
unknown <jdecotpro@gmail.com>
2021-08-02 10:11:06 +0200

    ajout du commentaire hello peggy

--- fichier2.py
+++ fichier2.py
@@ -1,4 +1,5 @@
 numero_de_fichier = 1
-numero_de_utilisateur = 0
+numero_de_utilisateur = 2
+# Salut Peggy !
 print(f"Hello, je suis le fichier {numero_de_fichier}")
 print(f"Je suis l'utilisateur {numero_de_utilisateur}")
