e9845eb5bb5798b06ea88338c6be11b8c6164fa6
pparroche <peggy.parroche@gmail.com>
2021-08-02 09:46:02 +0200

    je suis l'utilisateur 1 peggy

--- fichier1.py
+++ fichier1.py
@@ -1,5 +1,5 @@
 numero_de_fichier = 1
-numero_de_utilisateur = 0
+numero_de_utilisateur = 1
 print(f"Hello, je suis le fichier {numero_de_fichier}")
 print(f"Je suis l'utilisateur {numero_de_utilisateur}")
 
